82955f56d7864fa1802c5a133df10850516b74a2
Rakesh <rakeshar3796@gmail.com>
2022-12-05 13:31:16 +0530

    package changes

--- ios/Plugin/VoiceRecorder.swift
+++ ios/Plugin/VoiceRecorder.swift
@@ -82,7 +82,24 @@ public class VoiceRecorder: CAPPlugin {
         if(customMediaRecorder == nil) {
             call.reject(Messages.RECORDING_HAS_NOT_STARTED)
         } else {
-            call.resolve(ResponseGenerator.fromBoolean(customMediaRecorder?.pauseRecording() ?? false))
+            let audioFileUrl = customMediaRecorder?.getOutputFile()
+            if(audioFileUrl == nil) {
+                customMediaRecorder = nil
+                call.reject(Messages.FAILED_TO_FETCH_RECORDING)
+                return
+            }
+            let recordData = RecordData(
+                recordDataBase64: readFileAsBase64(audioFileUrl),
+                mimeType: "audio/aac",
+                msDuration: getMsDurationOfAudioFile(audioFileUrl)
+            )
+            // customMediaRecorder = nil
+            if recordData.recordDataBase64 == nil || recordData.msDuration < 0 {
+                call.reject(Messages.FAILED_TO_FETCH_RECORDING)
+            } else {
+                call.resolve(ResponseGenerator.dataResponse(recordData.toDictionary()))
+            }   
+            // call.resolve(ResponseGenerator.fromBoolean(customMediaRecorder?.pauseRecording() ?? false))
         }
     }
     
